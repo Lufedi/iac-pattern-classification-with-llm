33edf07b96e59685fcc27a0cf10ea0daf0010a44
Felipe Diaz <lfeliped@amazon.com>
2023-03-14 21:35:29 -0600

    initial commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+
+
+REPO_DIR = '/Users/lfeliped/pipe/master/experiments/An-analysis-of-diagram-images-on-Git-repositories'
+LABELS_FILE = '/Users/lfeliped/pipe/master/experiments/An-analysis-of-diagram-images-on-Git-repositories/labeling/labels.txt'
+TRAIN_FOLDER = '/Users/lfeliped/temp/pipeline'
+DATASET = 'my_dataset'
+
+
+CODE2VEC_DIR = '/Users/lfeliped/pipe/master/premodels/code2vec'
+
+
+TRAIN_DIRS = ['train', 'test', 'val']
